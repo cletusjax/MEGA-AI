7c707f1b8b9b578d8ed00a5236d83db6910659b1
cletusjax <jaxc29163@gmail.com>
2025-01-31 15:46:56 +0100

    Update config.js

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ for (let i = 0; i < ownerlist.length; i++) {
 //
 global.botname = process.env.BOTNAME || 'MEGA-AI';
 global.pairingNumber = process.env.BOT_NUMBER || '23407076849343' ;  // put your number here
-global.SESSION_ID = process.env.SESSION_ID || 'REJZXPVG' ;  // put your session id here
+global.SESSION_ID = process.env.SESSION_ID || 'jEIRiC7T#1zPWk_9kHS836tawO7WBapM1Mx-jZwMvnRPkBKfDPt8' ;  // put your session id here
 
 global.mods = []
 global.prems = []
